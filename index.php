1d8b15b97e0c7429ac65bac58b1695bfcbc17f68
Willian Dutras <willian_dutras@hotmail.com>
2022-03-31 17:33:52 -0300

    new update html

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/main.css?v1.01">
+  <link rel="stylesheet" href="assets/css/main.css?v1.02">
   <title>Bradesco Open Box - Powered by Capgemini</title>
 </head>
 <body>
